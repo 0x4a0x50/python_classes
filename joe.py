84115d24e3474dca0af5fc2c92377540c0ef8dd0
JP <47210401+photoshoppedtodeath@users.noreply.github.com>
2021-12-27 15:58:18 +0000

    create an object of class Person

---
+++ joe.py
@@ -0,0 +1,11 @@
+import person
+
+joe = Person('joe',
+             34,
+             182,
+             104,
+             'male',
+             'student')
+
+print(joe)
+print(joe.get_birthdate())
